64b05a41f0fda7d5601b4178226dabc1871d63db
nbdaaron <aaronkau@nwk-134-34.nwk.ruw.rutgers.edu>
2016-10-23 06:56:09 -0400

    change to response 2

--- index.js
+++ index.js
@@ -222,18 +222,19 @@ app.get("/showtimes", function(request, response){
   };
 
   cronofy.readEvents(options)
-    .then(function (response) {
-        for (var counter=0;counter<response.events.length;counter++) {
+    .then(function (response2) {
+        for (var counter=0;counter<response2.events.length;counter++) {
           allEvents[2] = 5;
           allEvents[oc]= oc;
           oc++;
-          console.log(response.events[counter].start);
-          console.log(response.events[counter].end);
+          console.log(response2.events[counter].start);
+          console.log(response2.events[counter].end);
           console.log(allEvents[allEvents.length] + "hi");
           response.send(allEvents);
           response.end();
         }
     });
+
   }
 
 
