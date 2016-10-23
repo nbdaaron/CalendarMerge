eb116892b0553e2f34c09b57be207d4ceb39ee97
nbdaaron <aaronkau@nwk-134-34.nwk.ruw.rutgers.edu>
2016-10-22 22:16:22 -0400

    Allow Heroku to dynamically set port

--- index.js
+++ index.js
@@ -96,7 +96,9 @@ app.get("/showtimes", function(request, response){
 
 });
 
-app.listen(8080);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 function makeid()
 {
